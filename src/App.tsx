f5f61edfb22282a4d1040b06ee968c7f7e9dea01
Matt Simpson <70198997+MattiHel85@users.noreply.github.com>
2023-10-25 16:46:11 +0300

    fetch team data using react query and display it

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
 import './App.css'
+import { useQuery } from 'react-query';
 
-function App() {
-  const [count, setCount] = useState(0)
+const fetchData = async () => {
+  const res = await fetch('https://teamfinder.onrender.com/teams');
+  const data = await res.json();
+  return data;
+} 
+
+const App: React.FC = () => {
+  const {data, isLoading, isError} = useQuery('data', fetchData);
+
+  if(isLoading){
+    return <div>Loading...</div>
+  }
+
+  if(isError){
+    return <div>Error loading data</div>
+  }
 
   return (
     <>
       <h1>
         Welcome to Teamfinder 2.0
       </h1>
+        {data && data.map((item: any) => 
+          <ul key={item.id}>
+            <li>Team: {item.name}</li>
+            <li>Nickname: {item.nickname}</li>
+            <li>Founded: {item.founded}</li>
+            <li>Stadium: {item.groundName}</li>
+            <li>Stadium capacity: {item.groundCapacity}</li>
+            <li>Country: {item.country}</li>
+            <li>League: {item.league}</li>
+            <li>Coach: {item.coach}</li>
+          </ul>)}
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+// import React from 'react'
+import { QueryClient, QueryClientProvider} from 'react-query'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <App />
-  </React.StrictMode>,
+  </QueryClientProvider>
 )
